f34fd584285349693f73e5fcfc7ee421ad193c02
liutianzhao <email_ltz@163.com>
2021-03-21 01:30:07 +0800

    添加test01.java

--- src/com/ltz/test01.java
+++ src/com/ltz/test01.java
@@ -8,7 +8,7 @@ package com.ltz;
  */
 public class test01 {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test main");
         System.out.println("test2");
     }
 }
