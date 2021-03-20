db4c5b5b6a124bd36e26cc3318ad02235f27e4fc
liutianzhao <email_ltz@163.com>
2021-03-21 02:26:54 +0800

    c1更新三次main.java

--- src/com/ltz/Main.java
+++ src/com/ltz/Main.java
@@ -3,6 +3,6 @@ package com.ltz;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test main dev");
+        System.out.println("C1 test main dev");
     }
 }
