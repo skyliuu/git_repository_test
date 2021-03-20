d4f7b033e3beadc7e3b3121fbdc8bc97bb75d333
liutianzhao <email_ltz@163.com>
2021-03-21 00:54:53 +0800

    添加test01.java

---
+++ src/com/ltz/test01.java
@@ -0,0 +1,13 @@
+package com.ltz;
+
+/**
+ * @author liutianzhao
+ * @ClassName test01
+ * @Description TODO
+ * @createTime 2021/03/21 00:54
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
