c194e0a55459f964063930c27534143934fcda71
liutianzhao <email_ltz@163.com>
2021-03-21 02:18:19 +0800

    c1更新main.java

--- src/com/ltz/test02.java
+++ src/com/ltz/test02.java
@@ -10,5 +10,6 @@ public class test02 {
     public static void main(String[] args) {
         System.out.println("test2");
         System.out.println("test2");
+        System.out.println("test2");
     }
 }
